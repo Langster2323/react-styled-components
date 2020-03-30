5ac1027ece4b25d4a90fb67dfe56a391fed7039f
Langster2323 <langahkeem@gmail.com>
2020-03-29 20:27:03 -0500

    installed styled components

---
+++ styled-comp/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from "styled-components";
+
+const Heading = styled.h1`
+    color: red;
+    text-align: center;
+`
+
+const Home = () => {
+    return (
+        <div>
+            <Heading>Hello World</Heading>
+        </div>
+    )
+}
+
+export default Home
